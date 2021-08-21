16ba4ab7377ecebd6eaa0b78f5967b287d83b946
blackgem <blackgem@protonmail.com>
2021-08-20 23:49:24 -0500

    Update change_mac_address.py

--- change_mac_address.py
+++ change_mac_address.py
@@ -14,4 +14,4 @@ subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 subprocess.call(["ifconfig", interface, "up", new_mac])
 
 print ("Done!")
-subprocess.call("ifconfig en0", shell=True)
+subprocess.call("ifconfig" + interface, shell=True)
