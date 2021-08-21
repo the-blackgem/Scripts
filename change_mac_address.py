f5e5158cb293edb9ba41c0f4071f9ae36fd1d7b0
the-blackgem <85140688+the-blackgem@users.noreply.github.com>
2021-08-20 19:23:37 -0500

    Update change_mac_address.py

--- change_mac_address.py
+++ change_mac_address.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
+#Intended for Linux
+
 import subprocess
 
 subprocess.call("ifconfig en0 down", shell=True) #Use the network interface you want to change its MAC address
-#For Linux
 subprocess.call("ifconfig en0 hw ether 00:11:22:33:44:00", shell=True) #The new MAC address can be whatever you decide as long as it is 12 numbers separated by pairs with ":"
-#For Mac
-#subprocess.call("ifconfig en0 ether 00:11:22:33:44:00", shell=True)
 subprocess.call("ifconfig en0 up", shell=True)
 
 print ("Done!")
